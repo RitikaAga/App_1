70e6843f642ed19ff2de76495f3e38d5661da6d2
Ritika Agarwal <106179540+RitikaAga@users.noreply.github.com>
2023-07-04 15:16:13 +0530

    new

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let value1 = document.getElementById("head1");
 let value2 = document.getElementById("pera1");
 let value3 = document.getElementById("pera2");
 let value4 = document.getElementById("pera3");
+let value5 = document.getElementById("head2");
+let value6 = document.getElementById("para21");
+let value7 = document.getElementById("para22");
+let value8 = document.getElementById("para23");
+let value9 = document.getElementById("para24");
 
 function function1() {
     value1.innerText = "Personalized Catalogs and Pricing"; 
@@ -17,3 +22,20 @@ function function2() {
      value3.innerText = "Well placed Filters to provide refinement that produces only a single preference result";
      value4.innerText = "";
 }
+
+function function3() {
+    value5.innerText = "Strong Insights and Predictive Analytics"; 
+    value6.innerText = "Customized Sales / Purchase reports";
+     value7.innerText = "AI Based Predictive Reports for negotiations";
+    value8.innerText = "AI Based Predictive Reports for negotiations";
+     value9.innerText = "Extensive Data Analytics and Reports with Visualisation";
+}
+
+function function4() {
+    value5.innerText = "Quick and Smart Sales"; 
+    value6.innerText = "RFQ Management";
+     value7.innerText = "Trade Leads";
+    value8.innerText = "Forward and Spot Auction";
+     value9.innerText = "Catalog Marketing Management";
+}
+
